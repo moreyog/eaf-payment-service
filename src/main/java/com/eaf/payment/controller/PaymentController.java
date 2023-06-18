1981645f8ed73159f6f16078a609e9140184b6f4
Yogesh More <moreyog@gmail.com>
2023-06-18 23:31:39 +0530

    Payments service added back pre post auth role based

--- src/main/java/com/eaf/payment/controller/PaymentController.java
+++ src/main/java/com/eaf/payment/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.eaf.payment.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.access.prepost.PreAuthorize;
 //import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,12 +19,12 @@ public class PaymentController {
         return "Welcome this endpoint is not secure";
     }
     @GetMapping("/all")
-  //  @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public String getAllTheProducts() {
         return "Welcome Admin";
     }
     @GetMapping("/{id}")
-    //@PreAuthorize("hasAuthority('ROLE_USER')")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public String getProductById(@PathVariable int id) {
         return "Welcome User";
     }
